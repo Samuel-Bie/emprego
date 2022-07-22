5c2ab6b6c32bf8bc12e6039dbbaa440b217ae724
samuel-bie <samuelbiezinho@gmail.com>
2022-07-22 21:00:35 +0200

    Initial Commit

---
+++ database/boot.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE emprego;
+
+use emprego;
+CREATE TABLE `contacts` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `email` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  `message` text COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
---
+++ get_reponse.php
@@ -0,0 +1,21 @@
+<?php
+require_once("database.php");
+
+
+if ((isset($_POST['name']) && $_POST['name'] != '') && (isset($_POST['email']) && $_POST['email'] != '')) {
+    $yourName = $conn->real_escape_string($_POST['name']);
+    $yourEmail = $conn->real_escape_string($_POST['email']);
+    $message = $conn->real_escape_string($_POST['message']);
+
+
+    $sql = "INSERT INTO contact_form_info (name, email, message) VALUES ('" . $yourName . "','" . $yourEmail . "', '" . $message . "')";
+
+
+    if (!$result = $conn->query($sql)) {
+        die('There was an error running the query [' . $conn->error . ']');
+    } else {
+        echo "Thank you! We will contact you soon";
+    }
+} else {
+    echo "Please fill Name and Email";
+}
